005bb44283b138896c15a916efc6b45047812fd5
MavinSao <kavinkz70@gmail.com>
2020-04-06 13:50:38 +0700

    Updates

---
+++ precache-manifest.ee9582b2311438f798223917c4a2b9c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06947cff4af6e83baf2aea970c03bcb2",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "fc18c4c3ca5abf298db6",
+    "url": "/react-gh-pages/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "be2375188a1ef550e224",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc18c4c3ca5abf298db6",
+    "url": "/react-gh-pages/static/js/2.bfccb77f.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/react-gh-pages/static/js/2.bfccb77f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2375188a1ef550e224",
+    "url": "/react-gh-pages/static/js/main.0c2ac21a.chunk.js"
+  },
+  {
+    "revision": "34cf3eeb34598b8b1f4e",
+    "url": "/react-gh-pages/static/js/runtime-main.416907b2.js"
+  }
+]);
